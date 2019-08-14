24ad432b1fc8dbcc9ff559b95790e367854fc5ef
Becky Ghita <ghita109@gmail.com>
2019-08-13 20:30:41 -0700

    adding constant characters

---
+++ app/constants/constants.js
@@ -0,0 +1,32 @@
+exports.profiles = [
+    {
+        name: "Thor",
+        photo: "",
+        scores: [3,1,2,1]
+    },
+    {
+        name: "Superman",
+        photo: "",
+        scores: [2, 2, 1, 4]
+    },
+    {
+        name: "Hulk",
+        photo: "",
+        scores: [4, 3, 3, 3]
+    },
+    {
+        name: "Batman",
+        photo: "",
+        scores: [2, 4, 4, 4]
+    },
+    {
+        name: "Wonder Woman",
+        photo: "",
+        scores: [3, 2, 3, 4]
+    },
+    {
+        name: "Iron Man",
+        photo: "",
+        scores: [2, 4, 1, 2]
+    }
+]
