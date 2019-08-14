a272c58547259aad67f1e50b05e978d800bb8e19
Becky Ghita <ghita109@gmail.com>
2019-08-13 21:30:53 -0700

    added all characters to constants

--- app/constants/constants.js
+++ app/constants/constants.js
@@ -1,7 +1,7 @@
 exports.profiles = [
     {
         name: "Thor",
-        photo: "",
+        photo: "../../assets/images/Thor.jpg",
         scores: [3,1,2,1]
     },
     {
@@ -11,22 +11,22 @@ exports.profiles = [
     },
     {
         name: "Hulk",
-        photo: "",
+        photo: "../../assets/images/hulk.jpg",
         scores: [4, 3, 3, 3]
     },
     {
         name: "Batman",
-        photo: "",
+        photo: "../../assets/images/batman.jpg",
         scores: [2, 4, 4, 4]
     },
     {
         name: "Wonder Woman",
-        photo: "",
+        photo: "../../assets/images/wonderwoman.jpg",
         scores: [3, 2, 3, 4]
     },
     {
         name: "Iron Man",
-        photo: "",
+        photo: "../../assets/images/ironman.jpg",
         scores: [2, 4, 1, 2]
     }
 ]
